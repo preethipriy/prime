5c19fe09ad8162c9af0c36adbddf15c676d05fbb
preethipriy <34617823+preethipriy@users.noreply.github.com>
2018-01-21 12:01:45 +0530

    Create arms.c

---
+++ arms.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,t,res=0,rem;
+scanf("%d",&n);
+t=n;
+while(n!=0)
+{
+rem=n%10;
+res=res+d;
+n=n/10;
+}
+if(t==res)
+{
+printf("yes");
+}
+else
+{
+	printf("no");
+}
+}
